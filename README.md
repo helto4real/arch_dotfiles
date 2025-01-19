590c14b003349ab6ce08bdcf94f79b63c5fbf1e9
Tomas <tomas.hellstrom@yahoo.se>
2025-01-19 14:51:53 +0100

    document the final version

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # My arch linxu development environment
 
 This is my arch linux development and dotfile repository
+
+## Install
+
+```bash
+
+# download the install script
+
+curl  https://raw.githubusercontent.com/helto4real/arch_dotfiles/refs/heads/main/initiatl_install.sh | bash
+
+```
--- initiatl_install.sh
+++ initiatl_install.sh
@@ -1,11 +1,61 @@
+# Arch installation script selection
+# Boot loader: grub
+# Sound device: pipewire
 # 
+# Post install steps
+# Upgrade all packages
+sudo packman -Syu
+# Install some basic packages
+sudp pacman -Sy intel-ucode wget ntfs-3g neofetch lsusb usbutils
+
+# Desktop environment
+# This is the gnome/i3 combination
+sudo pacman -Sy --noconfirm xorg xinit i3 picom gnome dmenu rofi polybar gnome-tweaks feh
+
 # First install yay, google it
 # Update system with
-yay -Sy
-sudo pacman -Sy git neovim jq stow i3  
-yay -S picom
-yay -S tmux fzf
-yay -S rustup
+
+# 1. First install yay package manager
+sudo pacman -S --needed git base-devel linux-headers nano
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install nvidia drivers, see https://github.com/korvahannu/arch-nvidia-drivers-installation-guide
+#Enable multilib repository
+sudo nano /etc/pacman.conf
+# Uncomment the following lines by removing the # -character at the start them
+#        [multilib]
+#        Include = /etc/pacman.d/mirrorlist
+# Save the file with CTRL+S and close nano with CTRL+X
+# Run yay -Syu, to update the system package databasep
+yay -Syu
+yay -S nvidia-470xx-dkms nvidia-utils lib32-nvidia-utils
+
+# Done installing nvidia drivers
+
+# Fonts
+yay -Sy --noconfirm ttf-iosevka-nerd ttf-meslo-nerd ttf-noto-nerd noto-fonts-emoji noto-fonts-cjk 
+# Essential general packages for the os and utilities
+yay -Sy --noconfirm jq stow fzf ripgrep fd eza zoxide xclip firefox alacritty ghostty pavucontrol toilet imagemagick ffmpeg
+# Essential programming packages
+yay -Sy --noconfirm neovim tmux rustup nodejs npm bat sesh docker go python python-pip python-pynvim python-pipenv ansible github-cli bpytop lazygit
 rustup default stable
-yay -S nodejs npm
-yay -S pavucontrol # sound
+yay -Sy --noconfirm dotnet-sdk-9.0 dotnet-runtime-8.0
+
+yay -Sy --noconfirm yubikey-personalization-gui pcsc-tools libu2f-host yubikey-manager
+
+yay -Sy also-scarlet-gui
+
+# Clone dotfiles
+
+git clone https://github.com/helto4real/arch_dotfiles.git ~/.dotfiles
+
+# oh-my-bash
+source ~/.dotfiles/files/oh-my-bash/oh-my-bash.sh
+yay -Sy --noconfirm oh-my-bash-git
+
+# Apps, discord, facebook messenger (caprine)
+yay -Sy --noconfirm discord caprine obsidian 1password
+
+yay -S  # sound
