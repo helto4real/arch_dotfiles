9643449096870631a9fe5fd99dae4e52cb87d325
Tomas <tomas.hellstrom@yahoo.se>
2025-09-10 23:02:36 +0200

    fix click start apps in waybar

--- config/dotconfig/scripts/fast_menu.sh
+++ config/dotconfig/scripts/fast_menu.sh
@@ -4,7 +4,7 @@ daily_note="/home/thhel/.config/scripts/edit_daily_note.sh"
 toggle_vpn="/home/thhel/.config/scripts/toggle_wireguard_wrapper.sh"
 
 # Define the options for the power menu
-options="1. Daily Note\n2. Toggle VPN\nReboot\nLogout\nSuspend\nHibernate"
+options="1. Daily Note\n2. Toggle VPN\n3. Btop\nReboot\nLogout\nSuspend\nHibernate"
 
 # Show the menu using rofi
 selected_option=$(echo -e "$options" | rofi -dmenu -i -p "Fast menu:")
@@ -16,6 +16,9 @@ case $selected_option in
         ;;
     "2. Toggle VPN")
          $toggle_vpn
+        ;;
+    "3. Btop")
+         btop
         ;;
     Reboot)
         systemctl reboot
