03d9f23ebfcd61b7adb7a3a736914959dd6e2cf4
Tomas <tomas.hellstrom@yahoo.se>
2025-09-29 04:03:22 +0200

    old formatting

--- config/dotconfig/nvim_old/lua/helto4real/plugins/lualine.lua
+++ config/dotconfig/nvim_old/lua/helto4real/plugins/lualine.lua
@@ -1,15 +1,15 @@
 return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    event = {"InsertEnter", "CmdLineEnter"},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- See `:help lualine.txt`
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = 'catppuccin',
-            component_separators = '|',
-            section_separators = '',
-        },
-    },
+	-- Set lualine as statusline
+	"nvim-lualine/lualine.nvim",
+	event = { "InsertEnter", "CmdLineEnter" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- See `:help lualine.txt`
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "catppuccin",
+			component_separators = "|",
+			section_separators = "",
+		},
+	},
 }
